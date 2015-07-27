c2717172f16f1aef38fc645395c717e6c6a475bc
Adam Fluke & Nicholas Toulouse <pair+adamfluke.nicoto@devbootcamp.com>
2015-07-27 16:49:13 -0700

    worked our way through to create, show, and index a new article

---
+++ blog/app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    #render plain: params[:article].inspect
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
